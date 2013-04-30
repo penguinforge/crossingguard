b08c60c2a2bad96177a1a92379096474b1fd921d
Eric <eric@Lycan.nc.rr.com>
2013-04-29 22:10:05 -0400

    Basic CVE test for the openshift now that I have a working local example.

--- wsgi/myflaskapp.py
+++ wsgi/myflaskapp.py
@@ -1,10 +1,23 @@
-from flask import Flask
+from flask import Flask 
+from flask import render_template
+from flask import request
+
 app = Flask(__name__)
 
 @app.route("/")
-def hello():
-    return "Hello World!"
+def index():
+    return render_template('index.html')
+
+@app.route("/CVE/", methods=['GET', 'POST']) 
+def CVE_Report():
+    if request.method == 'POST':
+        return render_template('cve_report.html', cve=request.form['CVE'])
+
+    return '''
+            <form action="" method="post">
+               <p>CVE:<input type=text name=CVE> <input type=submit value="Get Report">
+            </form>
+           '''
 
 if __name__ == "__main__":
     app.run()
-
