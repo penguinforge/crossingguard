6d999f8d4e4bae1796cdb09ef890bd6e05faee02
Eric Rich <erich@fenrir.redhat.com>
2013-04-30 14:29:47 -0400

    Ok lets see if a working application up on this site.

--- wsgi/myflaskapp.py
+++ wsgi/myflaskapp.py
@@ -1,3 +1,6 @@
+import urllib2
+from BeautifulSoup import BeautifulSoup
+import re
 from flask import Flask, render_template, request, url_for
 
 app = Flask(__name__, static_url_path='/static/')
@@ -27,10 +30,37 @@ def CVE_Report():
 def IAVM_Report(iavm=None):
     if request.method == 'POST' or iavm:
         if iavm:
-            return render_template('iavm_report.html', iava=iavm)
+            iava = iavm 
         else:
-            return render_template('iavm_report.html', iava=request.form['IAVM'])
-    return '''
+            iava = request.form['IAVM']
+
+        base_uri = 'http://iase.disa.mil/stigs/downloads/xml/iavm-to-cve(u).xml'
+        xml = urllib2.urlopen(base_uri)
+        xmlsoup = BeautifulSoup(xml.read())
+        xmlsoup.originalEncoding
+
+        cves = []
+        iavms = xmlsoup.findAll('iavm')
+        for i in iavms:
+            if iava == re.findall(r'\"(.+?)\"', "{0}".format(i.s).split()[7])[0]:
+                #print iavm
+                #iava = i.s['iavm']
+                # If you want to see how an IAVA as xml is structured. print i
+                severity = i.s['severity']
+                iavm_cves = i.s.findAll('cvenumber')
+                for cve in iavm_cves:
+                    cves.append(cve.contents[0])
+                #if rhel_check and simple:
+                #    for cve in cves:
+                #    base_uri = 'https://access.redhat.com/security/cve/'
+                #    try:
+                #        uri = base_uri + cve
+                #        html = urllib2.urlopen(uri)
+                #        if html.geturl() == uri:
+
+        return render_template('iavm_report.html', iava=iava, severity=severity, cves=cves)
+    else:
+        return '''
             <form action="" method="post">
                <p>IAVM: <input type=text name=IAVM> <input type=submit value="Get Report">
             </form>
