cb3e5989aeea3269a296c46c721f2266d192a7ff
Eric Rich <erich@fenrir.redhat.com>
2013-04-30 18:02:50 -0400

    Up todate example, of what I am shooting to accomplish.

--- wsgi/myflaskapp.py
+++ wsgi/myflaskapp.py
@@ -25,7 +25,7 @@ def CVE_Report():
            '''
 
 
-@app.route("/IAVM/", methods=['GET', 'POST']) 
+@app.route("/IAVM/", methods=['POST']) 
 @app.route("/IAVM/<iavm>") 
 def IAVM_Report(iavm=None):
     if request.method == 'POST' or iavm:
@@ -40,31 +40,30 @@ def IAVM_Report(iavm=None):
         xmlsoup.originalEncoding
 
         cves = []
+        severity = "Not Found."
+
         iavms = xmlsoup.findAll('iavm')
         for i in iavms:
             if iava == re.findall(r'\"(.+?)\"', "{0}".format(i.s).split()[7])[0]:
-                #print iavm
-                #iava = i.s['iavm']
-                # If you want to see how an IAVA as xml is structured. print i
+                # If you want to see how an IAVA as xml is structured.
+                print i
                 severity = i.s['severity']
                 iavm_cves = i.s.findAll('cvenumber')
                 for cve in iavm_cves:
-                    cves.append(cve.contents[0])
-                #if rhel_check and simple:
-                #    for cve in cves:
-                #    base_uri = 'https://access.redhat.com/security/cve/'
-                #    try:
-                #        uri = base_uri + cve
-                #        html = urllib2.urlopen(uri)
-                #        if html.geturl() == uri:
+                    base_uri = 'https://access.redhat.com/security/cve/'
+                    try:
+                        uri = base_uri + cve.contents[0]
+                        html = urllib2.urlopen(uri)
 
+                        if html.geturl() == uri:
+                            cves.append({cve.contents[0]: True})
+                        else:
+                            cves.append({cve.contents[0]: False})
+                    except urllib2.HTTPError, e:
+                        print e.code
+                    except urllib2.URLError, e:
+                        print e.code
         return render_template('iavm_report.html', iava=iava, severity=severity, cves=cves)
-    else:
-        return '''
-            <form action="" method="post">
-               <p>IAVM: <input type=text name=IAVM> <input type=submit value="Get Report">
-            </form>
-           '''
 
 
 if __name__ == "__main__":
