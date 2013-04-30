96ee32ec82929898184cf01322b64d5570788a92
Eric Rich <erich@fenrir.redhat.com>
2013-04-30 08:57:22 -0400

    Fixing a favicon.ico error and expanding the tests on my IAVM report.
    Also trying to include BeautifulSoup (im going to need this).

--- wsgi/myflaskapp.py
+++ wsgi/myflaskapp.py
@@ -1,8 +1,6 @@
-from flask import Flask 
-from flask import render_template
-from flask import request
+from flask import Flask, render_template, request, url_for
 
-app = Flask(__name__)
+app = Flask(__name__, static_url_path='/static/')
 
 # Additional Logging for OpenShift
 app.config['PROPAGATE_EXCEPTIONS'] = True
@@ -11,6 +9,7 @@ app.config['PROPAGATE_EXCEPTIONS'] = True
 def index():
     return render_template('index.html')
 
+
 @app.route("/CVE/", methods=['GET', 'POST']) 
 def CVE_Report():
     if request.method == 'POST':
@@ -18,9 +17,25 @@ def CVE_Report():
 
     return '''
             <form action="" method="post">
-               <p>CVE:<input type=text name=CVE> <input type=submit value="Get Report">
+               <p>CVE: <input type=text name=CVE> <input type=submit value="Get Report">
             </form>
            '''
+
+
+@app.route("/IAVM/", methods=['GET', 'POST']) 
+@app.route("/IAVM/<iavm>") 
+def IAVM_Report(iavm=None):
+    if request.method == 'POST' or iavm:
+        if iavm:
+            return render_template('iavm_report.html', iava=iavm)
+        else:
+            return render_template('iavm_report.html', iava=request.form['IAVM'])
+    return '''
+            <form action="" method="post">
+               <p>IAVM: <input type=text name=IAVM> <input type=submit value="Get Report">
+            </form>
+           '''
+
 
 if __name__ == "__main__":
     app.run()
